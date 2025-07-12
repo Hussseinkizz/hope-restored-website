044637071c6ada7234f73b49bbc63188435378b2
Hussein Kizz ★ <hssnkizz@gmail.com>
2025-07-12 16:41:36 +0300

    Update vite.config.js
    remove host expose

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['hoperestoreduganda.org']
-  }
+  // server: {
+  //   allowedHosts: ['hoperestoreduganda.org']
+  // }
 })
